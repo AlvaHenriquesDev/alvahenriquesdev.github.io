b8699f4fae3920e37939d68064b6a485c774d398
Alvaro Henriques <alvahenriquesd@gmail.com>
2025-11-11 23:18:49 -0300

    Finalizada Apresentação e seção de tecnologias, assim como suas respectivas responsividades. Adicionados arquivos SVG das tecs e criado Toggle Light-Dark

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+const toggleTheme = document.getElementById('toggleTheme')
+const rootHtml = document.documentElement
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme")
+
+    currentTheme == "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark") // Operador Ternário
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
