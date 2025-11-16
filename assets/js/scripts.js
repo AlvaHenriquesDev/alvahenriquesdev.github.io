8546cf4b76416afa98d06958fc5f25d12e5e0e88
Alvaro Henriques <alvahenriquesd@gmail.com>
2025-11-15 23:58:57 -0300

    Projeto Base Finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Alternancia do Tema (Dark/Light)
 const rootHtml = document.documentElement
 const toggleTheme = document.getElementById('toggleTheme')
 
@@ -11,3 +12,24 @@ function changeTheme(){
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+// Dinamismo do Menu
+const menuLinks = document.querySelectorAll(".menu__link")
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    link.classList.add("active");
+  })
+})
+
+// Dinamismo do campo Sobre
+const accordionHeaders = document.querySelectorAll(".accordion__header")
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains('active')
+        accordionActive ? accordionItem.classList.remove("active"):accordionItem.classList.add("active")
+    })
+})
